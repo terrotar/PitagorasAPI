a7d59dbd766520315bf10a4524dc1a5bb471487e
terrotar <pedro_guedes93@hotmail.com>
2021-03-02 20:53:10 -0300

    Criado e integrado o Database usando SQLIte e sqlalchemy para o mapeamento(ORM). Necessário adaptar a route '/calculadora' para integrar ao sistema de banco de dados e as operações de CRUD. AInda necessita aparecer os resultados.

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 import math
 
 
+# Criação do app
 app = Flask("PitagorasAPI")
 
 
+# Implementação do Database, Sessão local e modelo de classes
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///sql_Pitagoras/sql_app.db"
+
+
+# Sessão Local
+db = SQLAlchemy(app)
+
+
+# Modelos do Database
+class Triangulo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cateto01 = db.Column(db.Float, nullable=False)
+    cateto02 = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"""
+                    Triangulo {self.id} criado com sucesso.
+                """
+
+
+# Usado para criação do Database
+db.create_all()
+
+
+# Endpoints
+# Route da Home Page
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
@@ -18,7 +47,7 @@ def calculadora():
     else:
         cateto01 = request.form['cateto01']
         cateto02 = request.form['cateto02']
-        calculo = math.hypot(int(cateto01), int(cateto02))
+        calculo = math.hypot(float(cateto01), float(cateto02))
         return f"""
         A hipotenusa do triângulo equilátero de catetos
         {cateto01} e {cateto02} é : {calculo:.2f}
